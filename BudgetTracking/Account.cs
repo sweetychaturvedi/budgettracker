34088a51e8898c2cf40b29246d380805643d9cd2
sweta sharma <chaturvedi.sweta41989@gmail.com>
2020-01-16 23:25:49 -0800

    Define classes

---
+++ BudgetTracking/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracking
+{
+  public  class Account
+    {
+        public string AccountType { get; set; }
+
+        public int AccountNumber { get; set; }
+
+        public string AccountName { get; set; }
+    }
+}
